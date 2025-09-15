2681ebfd0a496da2e6c311d20e43959f28c485ca
José_ <wevertonj187@gmail.com>
2025-09-15 20:49:38 -0300

    Alteração server

--- Api/server.js
+++ Api/server.js
@@ -1,15 +1,6 @@
-// api/server.js
 export default async function handler(req, res) {
   const DISCORD_USER_ID = process.env.DISCORD_USER_ID;
   const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
 
   if (req.method === "GET") {
     try {
@@ -19,18 +10,13 @@ export default async function handler(req, res) {
           headers: { Authorization: `Bot ${DISCORD_TOKEN}` },
         }
       );
-
-      if (!response.ok) {
-        return res.status(500).json({ error: "Erro ao buscar perfil no Discord" });
-      }
-
       const data = await response.json();
       return res.status(200).json(data);
-    } catch (error) {
+    } catch (err) {
       return res.status(500).json({ error: "Falha no servidor" });
     }
   } else {
     res.setHeader("Allow", ["GET"]);
-    return res.status(405).json({ error: `Método ${req.method} não permitido` });
+    return res.status(405).json({ error: "Método não permitido" });
   }
 }
